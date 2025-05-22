521d678216a8c0c7eb835db711f36f4847886502
Promchai Chooseang <promchai2sin@gmail.com>
2025-05-22 16:52:14 +0700

    feat: Initialize data science salaries prediction project

---
+++ src/ds_salaries_predict/__init__.py
@@ -0,0 +1,10 @@
+from ds_salaries_predict import config  # noqa: F401
+
+__docformat__ = "numpy"
+
+__doc__ = """
+ds_salaries_predict
+=================
+This package provides a set of tools for predicting salaries in the data science field.
+It includes data preprocessing, model training, and evaluation functionalities.
+"""
