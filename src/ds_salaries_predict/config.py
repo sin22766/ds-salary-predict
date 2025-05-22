5130b899b2945ad24b097cc39528df888526a92f
Promchai Chooseang <promchai2sin@gmail.com>
2025-05-22 18:15:53 +0700

    feat: add data exploration

--- src/ds_salaries_predict/config.py
+++ src/ds_salaries_predict/config.py
@@ -7,7 +7,7 @@ from loguru import logger
 load_dotenv()
 
 # Paths
-PROJ_ROOT = Path(__file__).resolve().parents[1]
+PROJ_ROOT = Path(__file__).resolve().parents[2]
 logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
 
 DATA_DIR = PROJ_ROOT / "data"
--- webapp/app.py
+++ webapp/app.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from ds-salaries-predict!")
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def hello_world():
+    return "Hello, World!"
