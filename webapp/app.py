f87c145571f69aee00ec39dc0d74c4cedd5d08f4
deffy316 <90592166+deffy316@users.noreply.github.com>
2025-05-22 21:51:16 +0700

    Add API endpoint

--- webapp/app.py
+++ webapp/app.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from ds-salaries-predict!")
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List, Optional
+
+app = FastAPI(title="DA Salary Predict")
 
 
-if __name__ == "__main__":
-    main()
+class Input(BaseModel):
+    data: List[float]
+
+class Output(BaseModel):
+    result: float
+
+
+@app.get("/", tags=["Health"])
+def read_root():
+    return {"message": "Welcome to the FastAPI app"}
+
+@app.get("/predict",response_model=Output)
+def predict(input: Input):
+    result = {"result": input.data[0]+100}
+    # result = {"result": 0.123123}
+    return result
